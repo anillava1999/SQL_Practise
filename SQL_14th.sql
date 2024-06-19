9d913d452014dacfea382cdcd4ec65fcc8724d19
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2024-06-19 12:48:15 +0530

    Create SQL_14th.sql

---
+++ SQL_14th.sql
@@ -0,0 +1,8 @@
+/*
+Show first name and last name of patients that weight within the range of 100 to 120 (inclusive)
+*/
+
+
+SELECT first_name, last_name
+FROM patients
+where weight between 100 and 120
