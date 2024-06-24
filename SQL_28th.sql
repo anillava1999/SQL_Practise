bec2dcde8d6409a874654791d3953b3803d0f04c
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2024-06-24 16:04:25 +0530

    Create SQL_28th.sql

---
+++ SQL_28th.sql
@@ -0,0 +1,12 @@
+/*
+
+Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+
+Primary diagnosis is stored in the admissions table.
+*/
+
+select patient_id, first_name, last_name
+from patients
+where patient_id in ( select patient_id
+        	from admissions
+          where diagnosis='Dementia')
