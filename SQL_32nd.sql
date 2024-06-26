3dc2e5af23ace9e49057c2f1b065d2a2d217bd4e
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2024-06-26 20:29:02 +0530

    Create SQL_32nd.sql

---
+++ SQL_32nd.sql
@@ -0,0 +1,8 @@
+/*
+We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
+EX: SMITH,jane
+
+*/
+select concat(Upper(last_name),",", lower(first_name))
+from patients
+order by first_name desc
